1870e77e5956ee83dfc7b2530a67f9fe31ee3741
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2022-10-30 20:18:00 -0300

    feat: add inital commit

---
+++ .eslintrc.js
@@ -0,0 +1,100 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json"],
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "eslint-plugin-import-helpers",
+    "eslint-plugin-import",
+    "eslint-plugin-unused-imports",
+  ],
+  rules: {
+    "import/no-unresolved": "error",
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "interface",
+        format: ["PascalCase"],
+        custom: {
+          regex: "^I[A-Z]",
+          match: true,
+        },
+      },
+    ],
+    "class-methods-use-this": "off",
+    "import/prefer-default-export": "off",
+    "no-shadow": "error",
+    "no-console": "off",
+    "no-useless-constructor": "off",
+    "no-empty-function": "off",
+    "lines-between-class-members": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "import-helpers/order-imports": [
+      "error",
+      {
+        newlinesBetween: "always",
+        groups: [
+          "module",
+          ["/^@(components)/"], // add here the order imports
+          ["parent", "sibling", "index"],
+        ],
+        alphabetize: { order: "asc", ignoreCase: true },
+      },
+    ],
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: ["**/*.spec.js"],
+      },
+    ],
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".jsx", ".tsx"],
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    eqeqeq: "error",
+    semi: ["error", "always"],
+    "semi-spacing": "error",
+    "semi-style": ["error", "last"],
+    "eol-last": "error",
+    "array-bracket-spacing": ["error", "never"],
+    "no-multi-spaces": "error",
+    "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+    "unused-imports/no-unused-imports": "error",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+};
